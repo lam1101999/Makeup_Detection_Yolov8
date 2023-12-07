5df0de4875d67dcb59c912a4a07a496d22bd8d2d
VinhRP <152880120+VinhRP@users.noreply.github.com>
2023-12-07 16:56:18 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,6 @@ from PIL import Image
 image_file = st.file_uploader("Upload an image", type=["png", "jpg", "jpeg"])
 if image_file is not None:
   our_image = Image.open(image_file).convert("RGB")
-st.text(information)
+
 st.image(our_image, channels="RGB")
 
