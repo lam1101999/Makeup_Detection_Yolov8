2b52884a37f8aa27e3b30ac9d70a6dbca6d99456
VinhRP <152880120+VinhRP@users.noreply.github.com>
2023-12-07 16:37:26 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,18 @@
 import streamlit as st
+from PIL import Image
+from torchvision.models import resnet50, ResNet50_Weights
+from torchvision.models._api import WeightsEnum
+from torch.hub import load_state_dict_from_url
+import torch
+import torchvision
+from torchvision import transforms
+from typing import List, Tuple
+import ultralytics
+ultralytics.checks()
+from ultralytics import YOLO
 
-st.write('Hello world!')
+image_file = st.file_uploader("Upload an image", type=["png", "jpg", "jpeg"])
+if image_file is not None:
+  our_image = Image.open(image_file).convert("RGB")
+  yolo = YOLO('./face_detection_224.pt')
+  results = yolo.predict(our_image) # predict on an image
