78f05c159d19ed0d81dca1b605a409423173a9d2
VinhRP <152880120+VinhRP@users.noreply.github.com>
2023-12-07 16:45:06 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
+!pip install -q streamlit
+import locale
+locale.getpreferredencoding = lambda: "UTF-8"
+
 import streamlit as st
 from PIL import Image
 from torchvision.models import resnet50, ResNet50_Weights
@@ -8,11 +12,11 @@ import torchvision
 from torchvision import transforms
 from typing import List, Tuple
 import ultralytics
-ultralytics.checks()
 from ultralytics import YOLO
 
 image_file = st.file_uploader("Upload an image", type=["png", "jpg", "jpeg"])
 if image_file is not None:
   our_image = Image.open(image_file).convert("RGB")
-  yolo = YOLO('./face_detection_224.pt')
-  results = yolo.predict(our_image) # predict on an image
+st.text(information)
+st.image(our_image, channels="RGB")
+
