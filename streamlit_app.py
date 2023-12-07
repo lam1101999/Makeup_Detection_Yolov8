58f91f2a15b94eb84692407e640deef4dddbb13d
VinhRP <152880120+VinhRP@users.noreply.github.com>
2023-12-07 20:24:27 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,19 @@
 import streamlit as st
 from PIL import Image
-from ultralytics import YOLO
+import io
+import requests
+import helper.image_helper as ih
+import torch
+import torch.nn as nn
+import torchvision.models as models
+
+model_weight_url = 'https://github.com/VinhRP/Makeup_Detection_Yolov8/releases/download/Makeup_detection/ResNet50.pth'
+response = requests.get(model_weight_url)
+model_weight_data = response.content
+parameters = torch.load(io.BytesIO(model_weight_data), map_location=torch.device('cpu'))  
+model = None
+model = models.resnet50(weights=None)
+model.load_state_dict(parameters)
 
 image_file = st.file_uploader("Upload an image", type=["png", "jpg", "jpeg"])
 if image_file is not None:
