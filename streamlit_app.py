515041a13e13d3fb3d7d873b7d62c84ab459165b
VinhRP <152880120+VinhRP@users.noreply.github.com>
2023-12-07 16:54:38 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,17 +1,5 @@
-import locale
-locale.getpreferredencoding = lambda: "UTF-8"
-
 import streamlit as st
 from PIL import Image
-from torchvision.models import resnet50, ResNet50_Weights
-from torchvision.models._api import WeightsEnum
-from torch.hub import load_state_dict_from_url
-import torch
-import torchvision
-from torchvision import transforms
-from typing import List, Tuple
-import ultralytics
-from ultralytics import YOLO
 
 image_file = st.file_uploader("Upload an image", type=["png", "jpg", "jpeg"])
 if image_file is not None:
