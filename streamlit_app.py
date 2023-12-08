f1ac930516aab40c7e88689bb67fdb35a6de647c
VinhRP <152880120+VinhRP@users.noreply.github.com>
2023-12-08 13:48:22 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,6 +45,8 @@ image_transform = transforms.Compose(
 res50.to(device)
 res50.eval()
 
+image_file = st.file_uploader("Upload an image", type=["png", "jpg", "jpeg"])
+
 if image_file is not None:
   our_image = Image.open(image_file).convert("RGB")
   with torch.inference_mode():
