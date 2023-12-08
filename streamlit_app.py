ee1a55d8994991f4554d2f4423bf7418eebb8171
VinhRP <152880120+VinhRP@users.noreply.github.com>
2023-12-08 13:55:46 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,6 +7,7 @@ import torch.nn as nn
 import torchvision.models as models
 from torchvision import transforms
 from typing import List, Tuple
+from ultralytics import YOLO
 
 
 model_weight_url = 'https://github.com/VinhRP/Makeup_Detection_Yolov8/releases/download/Makeup_detection/ResNet50.pth'
